f23a7193ae22952a661a51e8de776b82d0f569e2
slawkir <slawkir1984@gmail.com>
2021-09-05 00:49:21 +0400

    implement of function solveSudoku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 module.exports = function solveSudoku(matrix) {
   // your solution
+  const cell = [];
+    let doneMatrix;
+        
+
+    function empty(matrix) {
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                if (matrix[i][j] == 0) {
+                    cell[0] = i;
+                    cell[1] = j;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    function solMatrix(matrix) {
+        let i = 0,
+            j = 0;
+        if (empty(matrix)) {
+            i = cell[0];
+            j = cell[1];
+            for (let n = 1; n < 10; n++) {
+                if (checkRow(n, i, matrix) && checkColumn(n, j, matrix) && checkBox(n, i, j, matrix)) {
+                    matrix[i][j] = n;
+                    if (solMatrix(matrix)) {
+                        doneMatrix = matrix;
+                        return true;
+                    }
+                    matrix[i][j] = 0;
+                }
+            }
+            return false;
+        }
+        return true;
+    }
+    function checkRow(n, r, matrix) {
+        for (let i = 0; i < 9; i++) {
+            if (matrix[r][i] == n) {
+                return false;
+            }
+        }
+        return true;
+    }
+    function checkColumn(n, c, matrix) {
+        for (let i = 0; i < 9; i++) {
+            if (matrix[i][c] == n) {
+                return false;
+            }
+        }
+        return true;
+    }
+    function checkBox(n, r, c, matrix) {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (matrix[Math.floor(r / 3) * 3 + i][Math.floor(c / 3) * 3 + j] == n) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    solMatrix(matrix);
+    return doneMatrix;
 }
